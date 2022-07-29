6c909cd811605330f673642c404561e4c89a134d
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-29 18:24:12 -0300

    :sparkles: feat: define ProductService class

---
+++ src/services/Product.service.ts
@@ -0,0 +1,31 @@
+import Joi from 'joi';
+import validator from '../utils/validator';
+import Product from '../interfaces/Product.interface';
+import ProductModel from '../models/Product.model';
+
+export default class ProductService {
+  constructor(private model: ProductModel) {
+    this.model = model;
+  }
+
+  public validate = {
+    body: validator(
+      Joi.object({
+        name: Joi
+          .string()
+          .min(3)
+          .required(),
+        amount: Joi
+          .string()
+          .min(3)
+          .required(),
+      }),
+    ),
+  };
+
+  public async register(data: Product): Promise<Product> {
+    await this.validate.body(data);
+    const product = await this.model.register(data);
+    return product;
+  }
+}
