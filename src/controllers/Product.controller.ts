2c70c44096468795f2ff735a1bd39de26ea44975
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-29 18:24:33 -0300

    :sparkles: feat: define ProductController class

---
+++ src/controllers/Product.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import ProductService from '../services/Product.service';
+
+export default class ProductController {
+  constructor(private service: ProductService) {
+    this.service = service;
+    this.register = this.register.bind(this);
+  }
+
+  public async register(req: Request, res: Response): Promise<void> {
+    const product = await this.service.register(req.body);
+    
+    res.status(StatusCodes.CREATED).json(product);
+  }
+}
