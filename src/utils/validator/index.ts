ea5792c6b0de46b9112642b6a8e4df96c8ee0767
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-29 18:22:31 -0300

    :sparkles: feat: create validator helper

---
+++ src/utils/validator/index.ts
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+
+export default function validator(schema: Joi.Schema) {
+  return (data: object) => {
+    const { error, value } = schema.validate(data);
+    if (error) {
+      error.message = error.details[0].message.replace(/\[[0-9]*\]\./, '');
+      throw error;
+    }
+    return value;
+  };
+}
