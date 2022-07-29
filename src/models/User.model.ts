b0d7e3889bd64b5f9ade9f8c087fba2502fd815f
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-29 20:01:22 -0300

    :sparkles: feat: define UserModel

---
+++ src/models/User.model.ts
@@ -0,0 +1,37 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { User } from '../utils/types/User.types';
+
+export default class UserModel {
+  constructor(private connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async register(data: User): Promise<number> {
+    const sql = `
+      INSERT INTO Trybesmith.Users (username, classe, level, password)
+      VALUES (?, ?, ?, ?);
+    `;
+
+    const [{ insertId }] = await this.connection.query<ResultSetHeader>(sql, [
+      data.username,
+      data.classe,
+      data.level,
+      data.password,
+    ]);
+
+    return +insertId;
+  }
+
+  public async getByUsername(username: string): Promise<User> {
+    const sql = `
+      SELECT *
+      FROM Trybesmith.Users
+      WHERE username = ?
+    `;
+
+    const result = await this.connection.query(sql, [username]);
+    const [rows] = result;
+    const [user] = rows as User[] | [];
+    return user;
+  }
+}
