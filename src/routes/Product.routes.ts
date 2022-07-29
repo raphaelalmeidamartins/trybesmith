86381db45a0b53de33497c9df6f94734da793406
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-29 20:00:31 -0300

    :bug: fix: controller import

--- src/routes/Product.routes.ts
+++ src/routes/Product.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import 'express-async-errors';
-import productController from '../msc';
+import { productController } from '../msc';
 
 const productRoutes = Router();
 
