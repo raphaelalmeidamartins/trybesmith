a0705aa0170a7b2ac254e87ad9b743f659495c19
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-29 20:02:09 -0300

    :sparkles: feat: create users layers

--- src/msc.ts
+++ src/msc.ts
@@ -1,10 +1,20 @@
 import ProductController from './controllers/Product.controller';
+import UserController from './controllers/User.controller';
 import connection from './models/connection';
 import ProductModel from './models/Product.model';
+import UserModel from './models/User.model';
 import ProductService from './services/Product.service';
+import UserService from './services/User.service';
 
 const productModel = new ProductModel(connection);
 const productService = new ProductService(productModel);
 const productController = new ProductController(productService);
 
-export default productController;
+const userModel = new UserModel(connection);
+const userService = new UserService(userModel);
+const userController = new UserController(userService);
+
+export {
+  productController,
+  userController,
+};
