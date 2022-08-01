5b6592e32babc30445049a8eeda8a0ecc0c45b33
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-08-01 14:23:15 -0300

    :recycle: refactor: define Authentication service

--- src/msc.ts
+++ src/msc.ts
@@ -1,3 +1,4 @@
+import * as jwt from 'jsonwebtoken';
 import OrderController from './controllers/Order.controller';
 import ProductController from './controllers/Product.controller';
 import UserController from './controllers/User.controller';
@@ -5,20 +6,23 @@ import connection from './models/connection';
 import OrderModel from './models/Order.model';
 import ProductModel from './models/Product.model';
 import UserModel from './models/User.model';
+import AuthService from './services/Auth.service';
 import OrderService from './services/Order.service';
 import ProductService from './services/Product.service';
 import UserService from './services/User.service';
+
+const authService = new AuthService(jwt);
 
 const productModel = new ProductModel(connection);
 const productService = new ProductService(productModel);
 const productController = new ProductController(productService);
 
 const userModel = new UserModel(connection);
-const userService = new UserService(userModel);
+const userService = new UserService(userModel, authService);
 const userController = new UserController(userService);
 
 const orderModel = new OrderModel(connection);
-const orderService = new OrderService(orderModel);
+const orderService = new OrderService(orderModel, authService);
 const orderController = new OrderController(orderService);
 
 export {
