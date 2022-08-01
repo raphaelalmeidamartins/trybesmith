c8ba675af85b00bfc2424777d64e361bcaf7b32d
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-08-01 14:19:40 -0300

    :bug: fix: regexp

--- src/utils/validator/index.ts
+++ src/utils/validator/index.ts
@@ -4,7 +4,7 @@ export default function validator(schema: Joi.Schema) {
   return (data: object) => {
     const { error, value } = schema.validate(data);
     if (error) {
-      error.message = error.details[0].message.replace(/\[[0-9]*\]\./, '');
+      error.message = error.details[0].message.replace(/[[\]0-9]{3}/, '');
       throw error;
     }
     return value;
