d7d690e851ab8bdb0a6fbf077e8e85f1bad1d53a
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-08-04 21:54:31 -0300

    :books: docs: create Swagger documentation (in progress)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUI from 'swagger-ui-express';
 import loginRoutes from './routes/Login.routes';
 import orderRoutes from './routes/Order.routes';
 import productRoutes from './routes/Product.routes';
@@ -7,8 +8,23 @@ import userRoutes from './routes/User.routes';
 import authErrorMiddleware from './utils/middlewares/authError.middleware';
 import errorMiddleware from './utils/middlewares/error.middleware';
 import validationErrorMiddleware from './utils/middlewares/validationError.middleware';
+import swaggerSettingsBr from './swagger-br.json';
+import swaggerSettingsEn from './swagger-en.json';
 
 const app = express();
+
+const options = {};
+
+app.use(
+  '/docs/br',
+  swaggerUI.serveFiles(swaggerSettingsBr, options),
+  swaggerUI.setup(swaggerSettingsBr),
+);
+app.use(
+  '/docs/en',
+  swaggerUI.serveFiles(swaggerSettingsEn, options),
+  swaggerUI.setup(swaggerSettingsEn),
+);
 
 app.use(express.json());
 
